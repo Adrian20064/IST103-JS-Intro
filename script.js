fd0a7043bdce17cbdda2fca41ae8db455330121b
Adrian20064 <carrascoadrian2006@gmail.com>
2025-03-10 20:19:28 -0700

    1 COMMIT CLASS
    "firstname-class activity 2 - 10 March 2025 #1"

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function helloworld(){
-  alert("Hello World")
-}
+//function helloworld() {
+//  alert("Hello World");
+//}
+
+const Persona = "Adrian Carrasco";
+const age = "17";
+const school = true;
+
+console.log(typeof Persona);
+console.log(typeof age);
+console.log(typeof school);
+
+console.log("Person name:" + Persona);
